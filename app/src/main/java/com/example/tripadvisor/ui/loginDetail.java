12f71d9d9668410a934651363a50091c61935302
b <b>
2021-12-27 03:59:29 +0500

    commit 3

---
+++ app/src/main/java/com/example/tripadvisor/ui/loginDetail.java
@@ -0,0 +1,14 @@
+package com.example.tripadvisor.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_detail);
+    }
+}
